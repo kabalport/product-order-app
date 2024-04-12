6f73c53b800584fea4d1a9dcb317808b32af5bbc
kabalport <kabalport@gmail.com>
2024-04-13 01:08:09 +0900

    Initial commit

---
+++ src/main/java/com/example/productorderapp/ProductOrderAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.productorderapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductOrderAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductOrderAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/productorderapp/ProductOrderAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.productorderapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductOrderAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
