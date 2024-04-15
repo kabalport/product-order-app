ad932f9f5c3e632510488a28959d86a2a23d72ee
kabalport <kabalport@gmail.com>
2024-04-16 00:20:35 +0900

    init

--- src/test/java/com/example/productorderapp/CreateMemberTest.java
+++ src/test/java/com/example/productorderapp/CreateMemberTest.java
@@ -1,27 +1,89 @@
 package com.example.productorderapp;
 
-import org.approvaltests.Approvals;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class CreateMemberTest  {
-    // pojo 개발합니다.
-    // innerclass 로 개발합니다. 개발이 끝난뒤 파일을 이동시킬 것입니다.
-    // pk는 id로 설정합니다. auto increment입니다.
-    // uuid라는 칼럼도 만듭니다. 이 컬럼은 String 타입입니다. 그리고 자바로 생성합니다. 그리고 unique key로 설정합니다.
-    // name이라는 칼럼도 만듭니다. String 타입입니다. null로 설정합니다.
-    // email이라는 칼럼도 만듭니다. String 타입입니다. null로 설정합니다.
+    @Autowired
+    private CreateMember createMember;
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @BeforeEach
+    void setUp(){
+        createMember = new CreateMember();
+    }
+
+    private class CreateMemberFixture {
+        private final String name;
+        private final String email;
+
+        public CreateMemberFixture(String name, String email) {
+            this.name = name;
+            this.email = email;
+        }
+    }
+
+
 
     @Test
     @DisplayName("멤버를 생성합니다.")
     void createMember(){
+        // given
+        final String userId = "userId";
+        final String email = "email";
+        final MemberDTO.request request = new MemberDTO.request(userId, email);
 
+        // when : 로직수행
+        createMember.execute(request);
 
-
-        // then: 먼저 then 만듭니다.
-        assertEquals(1,1);
+        // then : 기대한 결과값과 동일합니다.
+        final Member member = memberRepository.findById(1L).get();
+        assertNotNull(member, "결과 null 확인");
+        assertEquals(member,new CreateMemberFixture("userId","email"),"예상과 결과가 동일한지 확인합니다.");
     }
 
+
+class Member {
+        private final String userId;
+        private final String email;
+
+
+        public Member(String userId, String email) {
+            this.userId = userId;
+            this.email = email;
+        }
+    }
+
+    private class CreateMember {
+        public Member execute(MemberDTO.request request) {
+
+            final String userId = request.userId();
+            final String email = request.email();
+
+            final Member member = new Member(userId,email);
+            memberRepository.save(member);
+        }
+    }
+
+    private class MemberDTO {
+        public static class request {
+            String userId;
+            String email;
+            public request(String userId, String email) {
+                this.userId = userId;
+                this.email = email;
+            }
+        }
+    }
+
+    private interface MemberRepository extends JpaRepository<Member,Long> {
+        Member save(MemberDTO.request member);
+    }
 }
---
+++ src/test/java/com/example/productorderapp/CreateProductTest.java
@@ -0,0 +1,19 @@
+package com.example.productorderapp;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+
+
+public class CreateProductTest {
+
+    @Test
+    @DisplayName("상품을 생성합니다.")
+    void createProduct(){
+
+        // then
+//        Assertions.assertEquals(expect,result);
+    }
+}
